3193e686be76549cf92e9b4f3e8c0dca1d403e1b
Rachit Gupta <rachit.gupta@jabong.com>
2014-07-10 16:13:30 +0530

    Initial Commit.

---
+++ src/skuvalidator/SKUValidatorApp.java
@@ -0,0 +1,44 @@
+/*
+ * SKUValidatorApp.java
+ */
+
+package skuvalidator;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class SKUValidatorApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new SKUValidatorView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of SKUValidatorApp
+     */
+    public static SKUValidatorApp getApplication() {
+        return Application.getInstance(SKUValidatorApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(SKUValidatorApp.class, args);
+    }
+}
